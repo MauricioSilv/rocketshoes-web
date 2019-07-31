d6bc24b15f6d4d623dc36bb4214ae67fdb47813e
MauricioSilv <mausilva828@gmail.com>
2019-07-31 11:20:15 -0300

    :rocket: Navigating the saga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer, Zoom } from 'react-toastify';
 import Routes from './routes';
@@ -7,17 +7,18 @@ import Header from './components/Header';
 import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
 
+import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <GlobalStyle />
         <Header />
         <Routes />
         <ToastContainer autoClose={3000} transition={Zoom} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
