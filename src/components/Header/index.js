7ddb60fb255468d3879c0221e08fed47b90730e8
MauricioSilv <mausilva828@gmail.com>
2019-08-06 23:22:38 -0300

    :art: Using react hooks

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { MdShoppingBasket } from 'react-icons/md';
 import logo from '../../assets/images/logo.svg';
 import { Container, Cart } from './styles';
 
-function Header({ cartLength }) {
+export default function Header() {
+  const cartLength = useSelector(state => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
@@ -22,7 +24,3 @@ function Header({ cartLength }) {
     </Container>
   );
 }
-
-export default connect(state => ({
-  cartLength: state.cart.length,
-}))(Header);
